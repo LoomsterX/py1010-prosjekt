e988155f11d831e0272f5662db7a6ad00968374b
Sondre Melbye <sondremelbye@hotmail.com>
2025-09-02 19:25:08 +0200

    comment

--- arbeidskrav/a-1/a-1.py
+++ arbeidskrav/a-1/a-1.py
@@ -10,6 +10,7 @@ bomavgift_el: float = 0.1               # kr/km
 bomavgift_bensin: float = 0.3           # kr/km
 std_kjorelengde: int = 10000            # km
 
+# Lage metode for vurdering 
 def vurdering(kjorelengde: int):
     # Beregner kostnad for elbil
     kostnad_el_fast = forsikring_el + trafikkforsikringsavgift*365
@@ -24,12 +25,13 @@ def vurdering(kjorelengde: int):
     # Vurdering
     differanse = kostnad_bensin_total - kostnad_el_total
     
+    # Printe resultat
     print(
         f"De årlige kostnadene basert på kjørelengde: {kjorelengde:,}km er: \
         \n \U0001F50B Elbil: {kostnad_el_total:,} kr \
         \n \U0001F6E2 Bensinbil: {kostnad_bensin_total:,} kr"
         )
-
+    
     if kostnad_el_total > kostnad_bensin_total:
         print(f"Du bør kjøpe bensinbil \U0001F6E2 Den er {abs(differanse):,} kr billigere i drift per år \U0001F4B0")
     else:
