425198f1fef401eb1234c12495bddca6eeb730a1
Sondre M <sondremelbye@hotmail.com>
2025-11-09 15:12:19 +0100

    minor changes

--- arbeidskrav/a-2/Oppgave6.py
+++ arbeidskrav/a-2/Oppgave6.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def funksjon(x):
-    return -x**2 - 5
+    return -(x**2) - 5
+
 
 intervall = np.linspace(-10, 10, 2000)
 y = funksjon(intervall)
@@ -12,8 +14,8 @@ if __name__ == "__main__":
     plt.title("Plot av funksjonen f(x) = -x^2 - 5")
     plt.xlabel("x")
     plt.ylabel("f(x)")
-    plt.axhline(0, color='black',linewidth=0.5, ls='--')
-    plt.axvline(0, color='black',linewidth=0.5, ls='--')
-    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.3)
-    plt.legend(["f(x) = -x^2 - 5"])
+    plt.axhline(0, color="black", linewidth=0.5, ls="--")
+    plt.axvline(0, color="black", linewidth=0.5, ls="--")
+    plt.grid(color="gray", linestyle="--", linewidth=0.3)
+    plt.legend(["f(x) = -x^2 - 5"], loc="upper right")
     plt.show()
