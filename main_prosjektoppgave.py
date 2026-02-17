e7fca1a69273ea9d2602196d1146353501f1754a
Sondre M <sondremelbye@hotmail.com>
2026-02-17 21:29:16 +0100

    Opprette problembeskrivelse og main_prosjektoppgave

---
+++ main_prosjektoppgave.py
@@ -0,0 +1,11 @@
+from Prosjektoppgave.utils import functions
+
+
+def main():
+    # Kall funksjoner i Prosjektoppgave/utils/functions.py her
+    # Eksempel: functions.example()
+    pass
+
+
+if __name__ == "__main__":
+    main()
